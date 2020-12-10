0f7bd03891d5881f1d02310b002678a84b2c694a
Kunhee Lee <kunheeuk@naver.com>
2020-12-10 10:25:10 +0900

    ConvertVideoToImages

---
+++ Practice_using_datas/8.ConvertVideoToImages.py
@@ -0,0 +1,29 @@
+import os
+import cv2 as cv
+
+try:
+    directory = "./datas/images/Imageframes"
+    os.makedirs(directory, exist_ok= True)
+    print("Directory {} created Successfully".format(directory))
+except OSError as error:
+    print("Directory {} can not be created".format(directory))
+
+def writeFrame(videocapture, second, cnt):
+    videocapture.set(cv.CAP_PROP_POS_MSEC, second*1000)
+    hasFrames, image = videocapture.read()
+    if hasFrames:
+        cv.imwrite(directory + "/image_" + str(cnt) + ".png", image)
+    return hasFrames
+
+
+filename = "./datas/videos/Armbot.mp4"
+cap = cv.VideoCapture(filename)
+sec = 0
+count = 0
+frameRate = 0.5
+
+success = cap.isOpened()
+while success:
+    sec = sec + frameRate
+    success = writeFrame(cap, sec, count)
+    count = count + 1
