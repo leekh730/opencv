8c343558abde03ebf501ad44b9615e3bb9420cfd
Kunhee Lee <kunheeuk@naver.com>
2020-12-10 13:14:47 +0900

    add_주석

--- Practice_using_datas/6.DrawLines_RectanglesCircles_Text.py
+++ Practice_using_datas/6.DrawLines_RectanglesCircles_Text.py
@@ -1,13 +1,17 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 
 img = np.zeros((512,512,3), np.uint8)
 print(img)
 
-img[:] = 255,0,0
+img[:] = 255,0,0 # 위 np.zeros(512,512,3)를 아래와 같이 풀어쓰는 것을 한 줄로 완료
+# img[:,:,0] = 255
+# img[:,:,1] = 0
+# img[:,:,2] = 0
 print(img.shape)
 
-cv.line(img, (0,0), (img.shape[1], img.shape[0]), (0,255,0), 3)
+cv.line(img, (0,0), (img.shape[1], img.shape[0]), (0,255,0), 3) # (img.shape[1], img.shape[0])중에서 img.shape[1]는 가로, img.shape[0]는 세로를 의미
 cv.rectangle(img, (0,0), (250, 350), (0,0,255), 2)
 
 cv.circle(img, (400,50), 30, (255, 255, 0), 5)
